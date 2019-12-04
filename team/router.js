0b13c54262eed0b262871bb15ab25ae12ce1e189
João Neves <joaoneves@MBPdeAnustupROY.localdomain>
2019-12-04 17:49:10 +0100

    Rest

--- team/router.js
+++ team/router.js
@@ -6,11 +6,17 @@ const router = new Router();
 router.get("/team", (req, res, next) => {
   Team.findAll()
     .then(teams => res.send(teams))
-    .catch(err => next(err));
-});
+    .catch(err => next(err))
+})
 
 router.post('/team', (req, res, next) => {
     Team.create(req.body)
+    .then(teams => res.send(teams))
+    .catch(err => next(err))
+})
+
+router.get('/team/:id', (req, res, next) => {
+    Team.findByPk(req.params.id)
     .then(teams => res.send(teams))
     .catch(err => next(err))
 })

fb09e89b65e6a90016a586716bf74e2547d1b185
João Neves <joaoneves@MBPdeAnustupROY.localdomain>
2019-12-04 17:25:10 +0100

    data

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const port = process.env.PORT || 4000;
 const teamRouter = require('./team/router')
+const bodyParser = require('body-parser')
+const app = express()
+const jsonParser = bodyParser.json()
 
-const app = express();
+app.use(jsonParser)
 app.use(teamRouter)
+
 app.listen(port, () => console.log(`Listening on ${port}`));
--- team/router.js
+++ team/router.js
@@ -6,7 +6,12 @@ const router = new Router();
 router.get("/team", (req, res, next) => {
   Team.findAll()
     .then(teams => res.send(teams))
-    .catch(err => next);
+    .catch(err => next(err));
 });
 
+router.post('/team', (req, res, next) => {
+    Team.create(req.body)
+    .then(teams => res.send(teams))
+    .catch(err => next(err))
+})
 module.exports = router

e3aa9a9b6b1951ee96989a259adc6acdb41d8fdf
João Neves <joaoneves@MBPdeAnustupROY.localdomain>
2019-12-04 15:55:49 +0100

    Router

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const port = process.env.PORT || 4000;
-const db = require("./db.js");
-const team = require("./team/model.js");
+const teamRouter = require('./team/router')
 
 const app = express();
-
+app.use(teamRouter)
 app.listen(port, () => console.log(`Listening on ${port}`));
